c9f9e40a31e00a4dcc7aaa6d55fd340e01fa7697
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-06-20 20:01:29 -0300

    Declared lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -19,10 +20,13 @@ public class Program {
 		
 		/*
 		 * list.stream(): Converte a Lista list para stream.
-		 * .map(): Aplica uma Função a todos elementos de uma stream e recebe como retorno o retorno do método nonStaticUpperCaseName() convertido em Lista. 
+		 * .map(): Aplica uma Função a todos elementos de uma stream e recebe parâmetro um Function, que neste caso será func,
+		 * que tem armazenado a expressão lambda declarada. 
 		 * .collect(Collectors.toList()): Converte uma stream em uma Lista.
 		 */
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 
 		names.forEach(System.out::println); // Utilizando o Reference method: System.out é a classe e println é o método.
 	}
