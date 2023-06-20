4236f2b98974ca1d33cce3d0aecbbc842fe74f55
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-06-20 20:04:26 -0300

    Inline lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -20,13 +19,10 @@ public class Program {
 		
 		/*
 		 * list.stream(): Converte a Lista list para stream.
-		 * .map(): Aplica uma Função a todos elementos de uma stream e recebe parâmetro um Function, que neste caso será func,
-		 * que tem armazenado a expressão lambda declarada. 
+		 * .map(): Aplica uma Função a todos elementos de uma stream e recebe parâmetro um Function, que neste caso é a própria expressão lambda declarada. 
 		 * .collect(Collectors.toList()): Converte uma stream em uma Lista.
 		 */
-		Function<Product, String> func = p -> p.getName().toUpperCase();
-
-		List<String> names = list.stream().map(func).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 
 		names.forEach(System.out::println); // Utilizando o Reference method: System.out é a classe e println é o método.
 	}
