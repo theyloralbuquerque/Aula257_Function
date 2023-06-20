8fd09be3cd52e6ee80fa6be343fd475d3e28abd9
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-06-20 19:48:31 -0300

    Method reference static

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 
@@ -20,12 +19,11 @@ public class Program {
 		
 		/*
 		 * list.stream(): Converte a Lista list para stream.
-		 * .map(): Aplica uma Função a todos elementos de uma stream e recebe como argumento um Function, que no nosso caso é uma instância
-		 * da nossa classe UpperCaseName, que implements um Function.
+		 * .map(): Aplica uma Função a todos elementos de uma stream e recebe como retorno o retorno do método staticUpperCaseName convertido em Lista. 
 		 * .collect(Collectors.toList()): Converte uma stream em uma Lista.
 		 */
 
-		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
 
 		names.forEach(System.out::println); // Utilizando o Reference method: System.out é a classe e println é o método.
 	}
